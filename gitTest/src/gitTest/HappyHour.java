e1376e49c37c10a1e3d21cc1e4fe31462dd92f00
sakoar <sakoar@lexmark.com>
2015-08-13 12:07:47 -0400

    initial checking of sample projct.

---
+++ gitTest/src/gitTest/HappyHour.java
@@ -0,0 +1,12 @@
+package gitTest;
+
+public class HappyHour {
+
+	public void sum (int x, int y) {
+		
+		int sum=x+y;
+		System.out.println("The sum is: "+sum);
+		
+	}
+
+}
---
+++ gitTest/src/gitTest/myGitTest.java
@@ -0,0 +1,17 @@
+package gitTest;
+
+import org.testng.annotations.Test;
+
+
+public class myGitTest {
+
+	HappyHour check = new HappyHour();
+		
+	@Test
+  public void testGit() {
+	  check.sum(10,30);
+	  System.out.println("Managing Git with Eclipse");
+	  
+	  
+  }
+}
